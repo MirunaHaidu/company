decd16ba4e9db93a3976c7e3ec036e2d5436199e
Miruna <miruna_haidu@yahoo.com>
2023-01-14 16:06:01 +0200

    Convertor

--- src/main/java/com/sda/company/service/CompanyService.java
+++ src/main/java/com/sda/company/service/CompanyService.java
@@ -1,4 +1,8 @@
 package com.sda.company.service;
 
+import com.sda.company.dto.CompanyCreateDto;
+import com.sda.company.dto.CompanyInfoDto;
+
 public interface CompanyService {
+    CompanyInfoDto createCompany(CompanyCreateDto companyCreateDto);
 }
